771334c9529a7fdc7322ce3392a5e2d42698183b
Ameen Elnaggar <153122493+AmeenElnaggar@users.noreply.github.com>
2025-02-10 17:26:33 +0200

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -71,7 +71,7 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-//##########################################################################################################
+//#########################################################################################################
 
 // Functions
 //-----------
